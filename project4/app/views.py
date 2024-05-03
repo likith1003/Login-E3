536cae9a7e5d2f18b1af2b61cdcb6e19ed1590da
likith1003 <123715227+likith1003@users.noreply.github.com>
2024-05-03 09:40:21 +0530

    Add files via upload

--- project4/app/views.py
+++ project4/app/views.py
@@ -23,12 +23,11 @@ def login(request):
     if request.method == 'POST':
         un = request.POST.get('un')
         pw = request.POST.get('pw')
-        UO = Detail.objects.all()
-        for user in UO:
-            if user.un == un:
-                if user.pw == pw:
-                    return HttpResponse('Logged in Successfull')
-                return HttpResponse('Invalid Password')
-        else:
-            return HttpResponse('User Not Found')
+        try:
+            UO = Detail.objects.get(un=un, pw = pw)
+            if UO:
+                d = {'user_object':UO}
+                return render(request, 'home.html', d)
+        except:
+            return HttpResponse('Invalid Credentials')
     return render(request, 'login.html')
